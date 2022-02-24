aeebcb605fe3b27f0acdb030e0b240f9786a5990
Techn0Ninja27 <66588357+Techn0Ninja27@users.noreply.github.com>
2022-02-24 11:02:15 +0600

    renamed and formatted conditional statements

--- python/conditional_statements.py
+++ python/conditional_statements.py
@@ -1,9 +1,9 @@
 # 1 & 2
 def ageCompare():
     '''compares two different ages'''
-    loopFinish = False #check for user error
-    while loopFinish == False: #keeps input loop running until correct input
-        try: #runs code and checks for error
+    loopFinish = False  # check for user error
+    while loopFinish == False:  # keeps input loop running until correct input
+        try:  # runs code and checks for error
             userName1 = str(input("enter name for person 1"))
             userAge1 = int(input("enter age for person 1"))
             userName2 = str(input("enter name for person 2"))
@@ -13,13 +13,14 @@ def ageCompare():
             print("input error")
             continue
         else:
-            #breaks loop
+            # breaks loop
             loopFinish = True
-    if userAge1 > userAge2: #if the first age is higher than the second
+    if userAge1 > userAge2:  # if the first age is higher than the second
         print(userName1, "is older than", userName2)
-    elif userAge1 < userAge2: #vice versa
+    elif userAge1 < userAge2:  # vice versa
         print(userName2, "is older than", userName1)
-    elif userAge1 == userAge2: #equal
+    elif userAge1 == userAge2:  # equal
         print(userName1, "is the same age as", userName2)
+
 
 ageCompare()
