6135251cadf0b2cda3068b46e33c8259b1787dd2
Techn0Ninja27 <66588357+Techn0Ninja27@users.noreply.github.com>
2022-03-03 11:12:26 +0600

    added comments

--- python/iterations.py
+++ python/iterations.py
@@ -5,15 +5,14 @@
 def int_input(message, errorMessage="Input Error"):
     '''takes integer input from user'''
     inputFailure = True  # check for failure, default True
-    while inputFailure:
-        try:
+    while inputFailure:  # while loop that only stops when an input is added
+        try:  # try code
             value = int(input(message))
-        except ValueError:
+        except ValueError:  # excepts an error
             print(errorMessage)
         else:
             break
     return value
-
 
 
 # for loop
@@ -21,10 +20,8 @@ def int_input(message, errorMessage="Input Error"):
 
 def two_powers(n):
     '''prints powers of 2 until n'''
-    for i in range(0, n):
+    for i in range(0, n):  # for loop
         print(2 ** (i + 1))
-
-
 
 
 # repeat probs
@@ -32,16 +29,23 @@ def two_powers(n):
 def repeat_two_powers():
     '''repeats two power function'''
     loopContinue = True
+    # this is basically a do while in python
+    # the check is created first, then the loop runs
     while loopContinue:
+        # an input for integer
         number = int_input("powers of two until", errorMessage="integer pls")
+        # prints the number chosen by the user
         print(f"powers of 2 until: {number}")
-        two_powers(number)
+        two_powers(number)  # two powers function
         print()
         userInput = input("would you like to play again")
         if userInput == "y":
-            continue
+            # continue skips the rest of the loop and jumps straight back to
+            # the test expression of the loop
+            continue  # i think this is whats meant by repeat, but not sure
         else:
             loopContinue = False
             break
+
 
 repeat_two_powers()
